be05c9d2811ec1b7b61827d98063f632bceeddec
pipopotamasu <ec0156hx@gmail.com>
2017-10-29 13:32:47 +0900

    WIP

---
+++ app/javascript/components/orders.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="orders">
+    <div class="input-form">
+      <el-button type="info" @click="addGame">Add Game</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+  data: function () {
+    return {
+      games: []
+    }
+  },
+  methods: {
+    addGame: function() {
+      Vue.set(this.games, this.members.games, { id: this.members.length + 1, order: this.members.length });
+    }
+  }
+}
+</script>
--- app/javascript/packs/baseball_order.js
+++ app/javascript/packs/baseball_order.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Hello from '../components/hello.vue'
+import Orders from '../components/orders.vue'
 import Members from '../components/members.vue'
 import Element from 'element-ui'
 
@@ -8,7 +8,7 @@ Vue.use(Element)
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/hello1', component: Hello },
+  { path: '/orders', component: Orders },
   { path: '/members', component: Members }
 ]
 
@@ -28,8 +28,8 @@ var app = new Vue({
   methods: {
     swichTab: function() {
       switch(this.activeName) {
-        case 'hello':
-          this.$el.querySelector('#hello').click()
+        case 'orders':
+          this.$el.querySelector('#orders').click()
           break;
         case 'members':
           this.$el.querySelector('#members').click()
