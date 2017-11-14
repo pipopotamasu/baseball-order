0b01864bcf53f7d52d213aad1522f5f3b3211d82
pipopotamasu <ec0156hx@gmail.com>
2017-11-14 21:20:38 +0900

    devided component

--- app/javascript/components/starting_members.vue
+++ app/javascript/components/starting_members.vue
@@ -8,20 +8,7 @@
       </thead>
       <draggable v-model="order.starting_members" :element="'tbody'" :options="{ animation: 200, group: 'order', filter: '.ignore-elements' }">
         <template v-for="(member, k) in order.starting_members" class="member">
-          <tr>
-            <td>{{ k + 1 }}</td>
-            <td>
-              <el-select v-model="member.position" placeholder="Select">
-              <el-option
-                v-for="item in order.positions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value">
-              </el-option>
-            </el-select>
-            </td>
-            <td>{{ member.name }}</td>
-          </tr>
+          <member :member="member" :positions="order.positions" :index="k"></member>
         </template>
         <tr class=".ignore-elements">
           <td></td>
@@ -34,57 +21,13 @@
 </template>
 
 <script>
+import Member from './starting_member.vue'
 import Draggable from 'vuedraggable'
 
 export default {
   props: ['order'],
-  data: function() {
-    return {
-      positions: [
-        {
-          value: 'P',
-          label: 'P'
-        },
-        {
-          value: 'C',
-          label: 'C'
-        },
-        {
-          value: 'FB',
-          label: 'FB'
-        },
-        {
-          value: 'SB',
-          label: 'SB'
-        },
-        {
-          value: 'TB',
-          label: 'TB'
-        },
-        {
-          value: 'SS',
-          label: 'SS'
-        },
-        {
-          value: 'LF',
-          label: 'LF'
-        },
-        {
-          value: 'CF',
-          label: 'CF'
-        },
-        {
-          value: 'RF',
-          label: 'RF'
-        },
-        {
-          value: 'DH',
-          label: 'DH'
-        },
-      ]
-    }
-  },
   components: {
+    Member,
     Draggable
   },
 }
