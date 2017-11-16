0e7ed473f6ea5200bfbfdfbaf3cf6c63ec76f79e
pipopotamasu <ec0156hx@gmail.com>
2017-11-16 19:52:19 +0900

    change text

--- app/javascript/components/sub_members.vue
+++ app/javascript/components/sub_members.vue
@@ -2,7 +2,7 @@
   <div class="sub-members box-card inline-box">
     <el-card>
       <div slot="header">
-        <span>Members</span>
+        <span>Sub Members</span>
       </div>
       <draggable v-model="order.sub_members" :element="'div'" :options="{ animation: 200, group: 'order', filter: '.ignore-elements' }">
         <div v-for="member in order.sub_members" class="member">
